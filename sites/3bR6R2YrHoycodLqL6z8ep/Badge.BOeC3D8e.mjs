d8b7e2d23cfba6ecc877b276bc981307a0efc722
Sourav Aggarwal <81032655+100RAV-AGGARWAL@users.noreply.github.com>
2026-02-15 18:03:40 +0530

    fixes

---
+++ sites/3bR6R2YrHoycodLqL6z8ep/Badge.BOeC3D8e.mjs
@@ -0,0 +1,9 @@
+import { t as e } from "./rolldown-runtime.DYJfJqco.mjs"; import { c as t, d as n, l as r, u as i } from "./react.XYFExCw3.mjs"; function a(e) {
+    return t => n(r, {
+        children: [i(`style`, {
+            children: `
+    #__framer-badge-container { display: none !important; }
+`}), i(e, { ...t })]
+    })
+} var o = e((() => { t() })); export { a as n, o as t };
+//# sourceMappingURL=Badge.BOeC3D8e.mjs.map
